3a9d92a00ff30a90381825463d00893d08652835
pangpang20 <676814828@qq.com>
2024-12-04 16:24:44 +0800

    更新依赖包

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -e
 
+# Upgrade pip and install hatch
+python3 -m pip install --upgrade --user pip
+python3 -m pip install --upgrade --user hatch
+
+# Verify hatch installation
+export PATH="$HOME/.local/bin:$PATH"
+hatch --version
+
+# Configure Git
+git config --global http.version HTTP/1.1
+
+# Create default environment
+hatch env create default
+
 # Clean previous builds
 rm -rf build/ dist/
-hatch build   
+
+# Build the project
+hatch build
